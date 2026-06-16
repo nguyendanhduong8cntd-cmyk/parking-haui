dc77d853255bf618c5215a5afebd511b2e4431d3
HuyDwin19 <vuhuy19032006@gmail.com>
2026-06-16 11:44:59 +0700

    test

--- 2_Project_QLCoiXeHaUI/src/parking/Parking.java
+++ 2_Project_QLCoiXeHaUI/src/parking/Parking.java
@@ -5,6 +5,7 @@ import view.Welcome;
 
 public class Parking {
     public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
         Welcome m1 = new Welcome();
         m1.setVisible(true);
     }
